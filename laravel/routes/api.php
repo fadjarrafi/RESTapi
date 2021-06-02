d1499515cb136c16dfbdf37a1f9fb388bfab2bec
Fadjar <dark.fir21@gmail.com>
2021-06-03 02:32:54 +0700

    Adding ProductController

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Product;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,13 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products', function () {
-    return Product::all();
-});
+Route::get('/products', [ProductController::class, 'index']);
 
-Route::get('/products', function () {
-    
-});
+Route::post('/products', [ProductController::class, 'store']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
